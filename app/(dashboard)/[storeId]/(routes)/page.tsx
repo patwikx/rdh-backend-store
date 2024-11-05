5d7359675b29fac921827add37b804c2897e8a4c
Patrick <plmiranda@rdretailgroup.com.ph>
2024-11-05 12:25:39 +0800

    minor fix on dashboard

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -10,6 +10,7 @@ import { formatter } from "@/lib/utils"
 import Overview from "@/components/overview"
 import { getPendingOrders } from "@/actions/get-pendingOrders"
 import PendingOrders from "@/components/recent-orders"
+import { getDeliveredOrder } from "@/actions/get-delivered-count"
 
 
 interface DashboardPageProps {
@@ -22,6 +23,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
   const totalRevenue = await getTotalRevenue(params.storeId)
   const graphRevenue = await getGraphRevenue(params.storeId)
   const salesCount = await getSalesCount(params.storeId)
+  const DeliveredCount = await getDeliveredOrder(params.storeId)
   const stockCount = await getStockCount(params.storeId)
   const pendingOrders = await getPendingOrders(params.storeId)
 
@@ -33,20 +35,20 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
         <div className="grid gap-4 grid-cols-4">
           <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
+              <CardTitle className="text-sm font-medium">Paid Orders</CardTitle>
               <DollarSign className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold">{formatter.format(totalRevenue)}</div>
+              <div className="text-2xl font-bold">{salesCount}</div>
             </CardContent>
           </Card>
           <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">Fulfilled Orders</CardTitle>
+              <CardTitle className="text-sm font-medium">Delivered Orders</CardTitle>
               <CreditCard className="h-4 w-4 text-muted-foreground" />
             </CardHeader>
             <CardContent>
-              <div className="text-2xl font-bold">{salesCount}</div>
+              <div className="text-2xl font-bold">{DeliveredCount}</div>
             </CardContent>
           </Card>
           <Card>
